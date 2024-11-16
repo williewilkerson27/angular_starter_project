f7bdd6743298d911c45772d74b44054310afb917
Wilky <williewilkerson27@gmail.com>
2024-11-15 20:31:25 -0500

    I learned about the @Input decorator and how it can help with a child receiving data and what kind of data to expect from a parent component, I also got the generic items to display and next I will apply CSS

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,13 +8,15 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo">
-      <h2 class="listing-heading"></h2>
-      
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
+//* @Input as used here allows us to receive data from the parent(housing-location.ts).
+//* The ! is there to let Angular know that every field shall have a value
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
